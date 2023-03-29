56382d78f29e6a4736d5d4d0cf40a7310b128471
burak.ugras <burakugras67@gmail.com>
2023-03-29 23:55:53 +0300

    ModelMapper is added

--- rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -2,33 +2,36 @@ package kodlama.io.rentACar.webApi.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/brands")
+@AllArgsConstructor
 public class BrandsController {
     private BrandService brandService;
 
-    @Autowired //@autowired : bütün uygulama içini tarar ve her kim BrandService 'i implemente ediyorsa onu çağırır yani onun new'lenmiş halini bize verir.
-    public BrandsController(BrandService brandService) {
-        this.brandService = brandService;
-    }
+    //@Autowired //@autowired : bütün uygulama içini tarar ve her kim BrandService 'i implemente ediyorsa onu çağırır yani onun new'lenmiş halini bize verir.
+   
     
-    @GetMapping("/getall")
+    @GetMapping()
     public List<GetAllBrandsResponse> getAll(){
         return brandService.getAll();
     }
 
-    @PostMapping("/add")
-    public void add(CreateBrandRequest createBrandRequest){
+    @PostMapping()
+    @ResponseStatus(code=HttpStatus.CREATED)
+    public void add(@RequestBody() CreateBrandRequest createBrandRequest){
         this.brandService.add(createBrandRequest);
     }   
 }
