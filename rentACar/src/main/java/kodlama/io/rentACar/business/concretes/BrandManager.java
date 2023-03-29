ba2efdbf4e26435a6a0ace29bc36e3592ee4db57
burak.ugras <burakugras67@gmail.com>
2023-03-29 23:54:21 +0300

    ModelMapper utility is added

--- rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -1,47 +1,51 @@
 package kodlama.io.rentACar.business.concretes;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.core.utilities.mappers.ModelMapperService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
-import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import lombok.AllArgsConstructor;
 
 @Service //bu sınıf bir business nesnesidir.
+@AllArgsConstructor
 public class BrandManager implements BrandService{
 
-    private BrandRepository brandRepository;    
-
-    @Autowired
-    public BrandManager(BrandRepository brandRepository) {
-        this.brandRepository = brandRepository;
-    }
+    private BrandRepository brandRepository; 
+    private ModelMapperService modelMapperService;
 
     @Override
     public List<GetAllBrandsResponse> getAll() {
         List<Brand> brands=brandRepository.findAll();
-        List<GetAllBrandsResponse> brandsResponse=new ArrayList<GetAllBrandsResponse>();
+        // List<GetAllBrandsResponse> brandsResponse=new ArrayList<GetAllBrandsResponse>();
 
-        for (Brand brand : brands) {
-            GetAllBrandsResponse responseItem=new GetAllBrandsResponse();
-            responseItem.setId(brand.getId());
-            responseItem.setName(brand.getName());
+        // for (Brand brand : brands) {
+        //     GetAllBrandsResponse responseItem=new GetAllBrandsResponse();
+        //     responseItem.setId(brand.getId());
+        //     responseItem.setName(brand.getName());
 
-            brandsResponse.add(responseItem);
-        }
+        //     brandsResponse.add(responseItem);
+        // }
+
+        List<GetAllBrandsResponse> brandsResponse=brands.stream()
+        .map(brand->this.modelMapperService.forResponse()
+        .map(brand,GetAllBrandsResponse.class)).collect(Collectors.toList());
 
         return brandsResponse;
     }
 
     @Override
     public void add(CreateBrandRequest createBrandRequest) {
-        Brand brand=new Brand();
-        brand.setName(createBrandRequest.getName());
+        // Brand brand=new Brand();
+        // brand.setName(createBrandRequest.getName());
+
+        Brand brand=this.modelMapperService.forRequest().map(createBrandRequest,Brand.class);
         
         this.brandRepository.save(brand);
     }
