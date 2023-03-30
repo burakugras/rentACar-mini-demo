cec21a9237d77d6981b7ae8e9d259838bfb6d2d2
burak.ugras <burakugras67@gmail.com>
2023-03-30 18:48:14 +0300

    New operations are added to Service layer

--- rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
@@ -31,7 +31,7 @@ public class BrandsController {
 
     @PostMapping()
     @ResponseStatus(code=HttpStatus.CREATED)
-    public void add(@RequestBody() CreateBrandRequest createBrandRequest){
+    public void add(@RequestBody CreateBrandRequest createBrandRequest){
         this.brandService.add(createBrandRequest);
     }   
 }
