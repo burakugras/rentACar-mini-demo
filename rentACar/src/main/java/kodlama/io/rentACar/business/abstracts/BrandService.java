745fe3f945943b2db44b5b3f5003bf09ebd90945
burak.ugras <burakugras67@gmail.com>
2023-03-30 18:47:46 +0300

    New operations are added to Service layer

--- rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -3,9 +3,14 @@ package kodlama.io.rentACar.business.abstracts;
 import java.util.List;
 
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 
 public interface BrandService {
     List<GetAllBrandsResponse> getAll();
     void add(CreateBrandRequest createBrandRequest);
+    void update(UpdateBrandRequest updateBrandRequest);
+    void delete(int id);
+    GetByIdBrandResponse getById(int id);
 }
--- rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 import kodlama.io.rentACar.core.utilities.mappers.ModelMapperService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
@@ -45,9 +47,27 @@ public class BrandManager implements BrandService{
         // Brand brand=new Brand();
         // brand.setName(createBrandRequest.getName());
 
-        Brand brand=this.modelMapperService.forRequest().map(createBrandRequest,Brand.class);
-        
+        Brand brand=this.modelMapperService.forRequest().map(createBrandRequest, Brand.class);
+
         this.brandRepository.save(brand);
+    }
+
+    @Override
+    public void update(UpdateBrandRequest updateBrandRequest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public GetByIdBrandResponse getById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
     }
     
 }
